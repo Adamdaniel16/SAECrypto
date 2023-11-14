7107c7485d7f4dceaba656aec860c69b71d2c2e4
Yohann Loiseau <yohann.loiseau@etu.univ-orleans.fr>
2023-11-14 18:24:50 +0100

    premier essai cassage_brutal (pas pu tester)

--- chiffrement_RSA.py
+++ chiffrement_RSA.py
@@ -1,7 +1,10 @@
 
 
 def cassage_brutal(message_chiffre, taille_cle1, taille_cle2):
-    ...
+    res = []
+    for i in range(2**min(taille_cle1, taille_cle2)):
+        res.append(message_chiffre**i)%min(taille_cle1, taille_cle2)
+    return res
 
 
 def cassage_astucieux(message_chiffre): #On peut ajouter des paramètres
