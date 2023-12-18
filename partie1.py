4655b6eac5ef1a375336cacd84691ef6d8502596
Adam.Daniel <adam-daniel.bin-zulkornain@etu.univ-orleans.fr>
2023-12-18 19:10:13 +0100

    chiffrement et déchiffrement de AES

--- partie1.py
+++ partie1.py
@@ -30,15 +30,16 @@ def transforme_liste(message_chiffre):
     return "".join(res)
 
 # les tests pour double chiffrement et déchiffrement
-with open("textes/arsene_lupin_extrait.txt", "r") as f:
-    cle1 = 0b110100001
-    cle2 = 0b011011010
-    msg = f.read()
-    cipher = chiffrer(cle1, cle2, transforme_msg(msg))
-    # print(cipher)
-    msg_clair = transforme_liste(dechiffrer(cle1, cle2, cipher))
-    # print(msg_clair)
-    print(msg_clair == msg)
+if __name__=="__main__":
+    with open("textes/arsene_lupin_extrait.txt", "r") as f:
+        cle1 = 0b110100001
+        cle2 = 0b011011010
+        msg = f.read()
+        cipher = chiffrer(cle1, cle2, transforme_msg(msg))
+        # print(cipher)
+        msg_clair = transforme_liste(dechiffrer(cle1, cle2, cipher))
+        # print(msg_clair)
+        print(msg_clair == msg)
 
 # retrouver les clés de chiffrement (toutes les possibilités)
 def cassage_brutal(message_clair, message_chiffre):
@@ -82,32 +83,33 @@ def cassage_astucieux(message_clair, message_chiffre):
     return None
 
 # les tests pour les fonctions pour retrouver les clés (cassage)
-with open("textes/arsene_lupin_extrait.txt", "r") as f:
-    # msg = f.read()
-    msg = "La vie est belle"
-    msg_list = transforme_msg(msg)
-    k1 = 10
-    k2 = 19
-    cipher = chiffrer(k1, k2, msg_list)
-    
-    # cassage brutal (Brute-Force Attack)
-    print("\nCassage Brutal:")
-    start = time.time()
-    brute_force = cassage_brutal(msg_list, cipher)
-    duree = time.time()-start
-    nbt = brute_force[2]
-    print((brute_force[0], brute_force[1]) == (k1,k2))
-    print(brute_force)
-    print(f"Durée: {duree:.5f} secondes")
-    print(f"Nombre de tentatives: {nbt} fois\n")
-    
-    # cassage astucieux
-    print("Cassage Astucieux:")
-    start = time.time()
-    astucieux = cassage_astucieux(msg_list, cipher)
-    duree = time.time()-start
-    nbt = astucieux[2]
-    print((astucieux[0], astucieux[1]) == (k1,k2))
-    print(astucieux)
-    print(f"Durée: {duree:.5f} secondes")
-    print(f"Nombre de tentatives: {nbt} fois\n")
+if __name__=="__main__":
+    with open("textes/arsene_lupin_extrait.txt", "r") as f:
+        # msg = f.read()
+        msg = "La vie est belle"
+        msg_list = transforme_msg(msg)
+        k1 = 10
+        k2 = 19
+        cipher = chiffrer(k1, k2, msg_list)
+        
+        ########## cassage brutal (Brute-Force Attack) ##########
+        print("\nCassage Brutal:")
+        start = time.time()
+        brute_force = cassage_brutal(msg_list, cipher)
+        duree = time.time()-start
+        nbt = brute_force[2]
+        print((brute_force[0], brute_force[1]) == (k1,k2))
+        print(brute_force)
+        print(f"Durée: {duree} secondes")
+        print(f"Nombre de tentatives: {nbt} fois\n")
+        
+        ########### cassage astucieux ##########
+        print("Cassage Astucieux:")
+        start = time.time()
+        astucieux = cassage_astucieux(msg_list, cipher)
+        duree = time.time()-start
+        nbt = astucieux[2]
+        print((astucieux[0], astucieux[1]) == (k1,k2))
+        print(astucieux)
+        print(f"Durée: {duree} secondes")
+        print(f"Nombre de tentatives: {nbt} fois\n")
